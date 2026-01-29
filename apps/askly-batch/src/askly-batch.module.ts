3943928dc11c64b837acb800560206faab37914c
komil14 <abdulvokhidovkomiljon@gmail.com>
2026-01-30 00:36:07 +0900

    feat: configure env veriables

--- apps/askly-batch/src/askly-batch.module.ts
+++ apps/askly-batch/src/askly-batch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AsklyBatchController } from './askly-batch.controller';
 import { AsklyBatchService } from './askly-batch.service';
+import {ConfigModule} from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AsklyBatchController],
   providers: [AsklyBatchService],
 })
--- apps/askly-batch/src/main.ts
+++ apps/askly-batch/src/main.ts
@@ -3,6 +3,6 @@ import { AsklyBatchModule } from './askly-batch.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AsklyBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
