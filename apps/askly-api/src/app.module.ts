9c16b49a368a0057e0e5cf7423addcdadd9e29c4
komil14 <abdulvokhidovkomiljon@gmail.com>
2026-01-30 18:02:36 +0900

    feat: restructure app module and add components and database modules

--- apps/askly-api/src/app.module.ts
+++ apps/askly-api/src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver } from '@nestjs/apollo';
+import {ConfigModule} from "@nestjs/config";
+import {GraphQLModule} from "@nestjs/graphql";
+import {ApolloDriver} from "@nestjs/apollo";
 import { AppResolver } from './app.resolver';
+import { ComponentsModule } from './components/components.module';
+import { DatabaseModule } from './database/database.module';
+
 @Module({
-	imports: [
-		ConfigModule.forRoot(),
-		GraphQLModule.forRoot({
+  imports: [ConfigModule.forRoot(),
+    GraphQLModule.forRoot({
       driver: ApolloDriver,
       playground: true,
       uploads: false,
-			autoSchemaFile: true,
-		}),
-	],
-	controllers: [AppController],
-	providers: [AppService, AppResolver],
+      autoSchemaFile: true,
+    }),
+    ComponentsModule,
+    DatabaseModule,],	
+  controllers: [AppController],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
